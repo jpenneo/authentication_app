6d82cbaa9cef01cb445ff7e84f7f43ec47883dcf
jpenneo <jpenneo@outlook.com>
2025-01-27 14:31:52 +0100

    Modificación_app

--- Backend/config.py
+++ Backend/config.py
@@ -10,14 +10,19 @@ class Config:
     DEBUG = os.getenv("FLASK_DEBUG", "True") == "True"
     ENV = os.getenv("FLASK_ENV", "development")
 
-    # Cargar credenciales de Firebase desde el archivo JSON
-    FIREBASE_CREDENTIALS_PATH = os.path.join(os.path.dirname(__file__), 'config', 'serviceAccountKey.json')
+    # Determinar las credenciales de Firebase
+    if os.getenv('FLASK_ENV') == 'production':
+        # En producción, las credenciales de Firebase vienen desde la variable de entorno
+        FIREBASE_CREDENTIALS = json.loads(os.getenv('FIREBASE_CREDENTIALS'))
+    else:
+        # En desarrollo, se usa el archivo local serviceAccountKey.json
+        FIREBASE_CREDENTIALS_PATH = os.path.join(os.path.dirname(__file__), 'config', 'serviceAccountKey.json')
+        try:
+            with open(FIREBASE_CREDENTIALS_PATH) as f:
+                FIREBASE_CREDENTIALS = json.load(f)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"El archivo de credenciales no se encuentra en la ruta: {FIREBASE_CREDENTIALS_PATH}")
 
-    try:
-        with open(FIREBASE_CREDENTIALS_PATH) as f:
-            FIREBASE_CREDENTIALS = json.load(f)
-    except FileNotFoundError:
-        raise FileNotFoundError(f"El archivo de credenciales no se encuentra en la ruta: {FIREBASE_CREDENTIALS_PATH}")
 
 # Inicialización de Firebase
 def initialize_firebase():
