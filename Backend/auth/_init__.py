d80006ae98b98eb3ed451f0c12f6d9e30e32b2b6
jpenneo <jpenneo@outlook.com>
2025-01-30 10:43:14 +0100

    v.1.0.0

--- Backend/auth/_init__.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-from config import Config
-
-cred = credentials.Certificate(Config.FIREBASE_CREDENTIALS)
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
-
--- Backend/generador_claves.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Generar una clave secreta de 24 bytes
-csrf_secret_key = os.urandom(24).hex()
-print(csrf_secret_key)
