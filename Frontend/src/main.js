b4f56cc57a96f6d828002c8612bd4be5da73e8fb
jpenneo <jpenneo@outlook.com>
2025-01-30 12:45:17 +0100

    v1.0.2

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-
+import axios from "axios";
 import { configureAxios } from "./utils/CSRF";
 
 // Configurar Axios
